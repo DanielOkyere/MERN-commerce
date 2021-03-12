1760319d1dba0951cdde718b4b0c9b804ed7a38b
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2021-03-12 07:40:11 +0000

    updated the validate request

--- ecom-back-end/src/routes/auth.js
+++ ecom-back-end/src/routes/auth.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
+const { check } = require("express-validator");
 const router = express.Router();
-const {signup, signin, requireSignin} = require('../controller/auth')
+const { signup, signin, } = require("../controller/auth");
+const { validateSignupRequest, isRequestValidated } = require("../validators/auth");
 
-
-router.post('/signup', signup)
-router.post('/signin', signin)
+router.post("/signup", validateSignupRequest, isRequestValidated, signup);
+router.post("/signin", signin);
 
 // router.post('/profile', requireSignin, (req, res) => {
 //     res.status(200).json({
@@ -13,5 +14,3 @@ router.post('/signin', signin)
 // })
 
 module.exports = router;
-
-
