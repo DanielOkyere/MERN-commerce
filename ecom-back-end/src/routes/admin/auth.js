0ef5537b31e8f17f943885aefe2969d4ae5574a7
Daniel Okyere <daniel.okyere@nexum.com>
2021-03-29 10:35:35 +0000

    Worked on the category page

--- ecom-back-end/src/routes/admin/auth.js
+++ ecom-back-end/src/routes/admin/auth.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {signup, signin,signout} = require('../../controller/admin/auth');
-const { requireSignin } = require('../../controller/common-middlewares');
-const { validateSignupRequest, isRequestValidated, validateSignInRequest } = require('../../validators/auth');
+const { signup, signin, signout } = require("../../controller/admin/auth");
+const { requireSignin } = require("../../controller/common-middlewares");
+const {
+  validateSignupRequest,
+  isRequestValidated,
+  validateSignInRequest,
+} = require("../../validators/auth");
 
-
-router.post('/admin/signup', validateSignupRequest, isRequestValidated, signup);
-router.post('/admin/signin', validateSignInRequest, isRequestValidated, signin);
-router.post('/admin/signout', requireSignin, signout);
-
+router.post("/admin/signup", validateSignupRequest, isRequestValidated, signup);
+router.post("/admin/signin", validateSignInRequest, isRequestValidated, signin);
+router.post("/admin/signout", signout);
 
 module.exports = router;
-
-
